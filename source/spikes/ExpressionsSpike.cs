9192ffa3c5ad76439fd3ff63398aa8d592def5b1
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2015-10-09 15:04:31 -0600

    Updated

--- source/spikes/ExpressionsSpike.cs
+++ source/spikes/ExpressionsSpike.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
+using code.features.catalog_browsing;
+using code.test_utilities;
 using developwithpassion.specification.adapters.rhino_mocks;
 using Machine.Specifications;
 
@@ -19,15 +22,63 @@ namespace code.spikes
 
         even_number(2).ShouldBeTrue();
 
+        var param = Expression.Parameter(typeof(int), "x");
         var number_2 = Expression.Constant(2);
+        var number_0 = Expression.Constant(0);
 
+        var mod_2 = Expression.Modulo(param, number_2);
+        var equal_0 = Expression.Equal(mod_2, number_0);
 
+        var complete = Expression.Lambda<Func<int, bool>>(equal_0,
+          param);
 
-
-        Func<int, bool> generated = ///
+        var generated = complete.Compile();
 
         generated(2).ShouldBeTrue();
       };
+
+      class Person
+      {
+        public string names { get; set; }
+      }
+
+      public class Token<Value>
+      {
+        string key;
+
+        public Token(string key)
+        {
+          this.key = key;
+        }
+
+        public Value map_from(IDictionary<string, string> data)
+        {
+          return default(Value);
+        } 
+      }
+
+      public interface IProvideApiBehaviour
+      {
+        IEnumerable<Department> get_departments();
+      }
+
+      public class ReportColumns
+      {
+        public static Token<string> name  = new Token<string>("name");
+        public static Token<DateTime> date  = new Token<DateTime>("date");
+      }
+
+      It get_the_name_of_an_attribute = () =>
+      {
+        var builder = Objects.expressions.to_target<Person>();
+        var raw_record = new Dictionary<string, string>();
+
+        var mapped_value = ReportColumns.name.map_from(raw_record);
+
+        var attribute_name = builder.attribute_name(x => x.names);
+        
+      };
+        
     }
   }
 }
--- source/test_utilities/Objects.cs
+++ source/test_utilities/Objects.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.IO;
+using System.Linq.Expressions;
 using System.Web;
+using developwithpassion.specifications.extensions;
 
 namespace code.test_utilities
 {
   public class Objects
   {
+    public class expressions
+    {
+      public static ExpressionBuilder<Item> to_target<Item>()
+      {
+        return new ExpressionBuilder<Item>();
+      }
+    }
     public class web
     {
       public static HttpContext create_http_context()
@@ -23,5 +32,13 @@ namespace code.test_utilities
         return new HttpResponse(new StringWriter());
       }
     }  
+  }
+
+  public class ExpressionBuilder<Item>
+  {
+    public string attribute_name(Expression<Func<Item, object>> accessor)
+    {
+      return accessor.Body.downcast_to<MemberExpression>().Member.Name;
+    }
   }
 }
